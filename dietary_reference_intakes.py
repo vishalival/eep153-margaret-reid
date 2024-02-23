4455021cd67c7bfe7e6d311367ebdf5674355d25
Vishali Vallioor <vishalivallioor@wifi-10-43-148-137.wifi.berkeley.edu>
2024-02-23 15:23:21 -0800

    modified dietary ref intakes again

--- dietary_reference_intakes.py
+++ dietary_reference_intakes.py
@@ -13,11 +13,11 @@ def inuit_dietary_recommendations(sex, age, age_unit_type='year'):
     '''
     merged_df = pd.read_csv("data/inuit_dietary_info.csv")
 
-    # Convert age to years if age_unit_type is 'month'
+    # Convert age to years if age_unit_type is 'month'.
     if age_unit_type == "month" or age_unit_type == "months":
         age = age / 12.0
 
-    # Adjust gender for age under 10
+    # Adjust gender for age under 10.
     if age <= 10:
         gender = 'ALL'
     else:
@@ -26,7 +26,8 @@ def inuit_dietary_recommendations(sex, age, age_unit_type='year'):
         elif sex in ["male", "m", "M", "Male"]:
             gender = 'M'
     
-    # Define the age group based on age in years
+    # Define the age group based on age in years.
+    # This section goes through the age to find the age required.
     age_group = '≤6 mo' if age <= 0.5 else \
                 '7-11 mo' if age <= 1 else \
                 '1-3 y' if age <= 3 else \
@@ -39,17 +40,17 @@ def inuit_dietary_recommendations(sex, age, age_unit_type='year'):
                 '51-70 y' if age <= 70 else \
                 '>70 y'
     
-    # Filter the merged_df for the specific age group and gender
+    # Filter the merged_df for the specific age group and gender.
     recommendations_df = merged_df[(merged_df['Age group'] == age_group) & (merged_df['Gender'] == gender)]
 
-    # Clean up the DataFrame by removing unnecessary columns
+    # Clean up the DataFrame by removing unnecessary columns.
     columns_to_remove = ['Unnamed: 0', 'index_x', 'index_y', 'index', 'Age group', 'Gender']
     recommendations_df = recommendations_df.drop(columns=[col for col in columns_to_remove if col in recommendations_df.columns])
 
-    # Format column names to be more readable
+    # Format column names to be more readable.
     recommendations_df.columns = [col.replace('\n', ' ').replace('­', '-').strip() for col in recommendations_df.columns]
 
-    # Convert the filtered DataFrame to a Series
+    # Convert the filtered DataFrame to a Series.
     recommendations_series = recommendations_df.squeeze()
 
     return recommendations_series
